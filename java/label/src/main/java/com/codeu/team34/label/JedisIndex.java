0a5ca6a9cba3292ccc5170c51d7f01585ea0c407
Myanna Harris <myharris13@gmail.com>
2016-08-09 15:58:41 -0700

    f

--- java/label/src/main/java/com/codeu/team34/label/JedisIndex.java
+++ java/label/src/main/java/com/codeu/team34/label/JedisIndex.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ArrayList.*;
 import java.util.HashMap;
 import java.util.HashSet;
